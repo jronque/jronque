d7c4f6d4e8a176ba65d0426f52a27c83ad80ecd3
Dmitry Avtonomov <dmt.avt@gmail.com>
2018-05-15 12:29:56 -0700

    Generator plugin starts making some sense

--- _plugins/category-indexer-plugin.rb
+++ _plugins/category-indexer-plugin.rb
@@ -1,7 +1,9 @@
 module Jekyll
-  class CategoryPage < Page
+  COLLECTION_TAXONOMY_LAYOUT_BASE_NAME = "collection-index"
+
+  class CollectionTaxonomyPage < Page
     def initialize(site, base, dir, category)
-      puts("Running category-indexer-plugin.rb initialize(site, base, dir, category) method.\n" +
+      puts("Running collection-indexer-plugin.rb initialize(site, base, dir, category) method.\n" +
         "   category=#{category}, base=#{base}, dir=#{dir}")
       @site = site
       @base = base
@@ -9,24 +11,24 @@ module Jekyll
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.read_yaml(File.join(base, '_layouts'), "#{COLLECTION_TAXONOMY_LAYOUT_BASE_NAME}.html")
       self.data['category'] = category
 
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      category_title_prefix = site.config['category_title_prefix'] || "#{category} Category: "
       self.data['title'] = "#{category_title_prefix}#{category}-yohoho"
     end
   end
 
-  class CategoryPageGenerator < Generator
+  class CollectionTaxonomyPageGenerator < Generator
     safe true
 
     def generate(site)
-      puts("Running category-indexer-plugin.rb generate(site) method")
-      if site.layouts.key? 'category_index'
+      puts("Running collection-indexer-plugin.rb generate(site) method")
+      if site.layouts.key? COLLECTION_TAXONOMY_LAYOUT_BASE_NAME
         puts("site.layouts.key == true")
         dir = site.config['category_dir'] || 'categories'
         site.categories.each_key do |category|
-          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+          site.pages << CollectionTaxonomyPage.new(site, site.source, File.join(dir, category), category)
         end
       else
         puts("site.layouts.key == false")
